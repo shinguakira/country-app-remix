d8bfb81754a53b4bb08f40987614910825f92fe2
AkiraShingu <shinguakira1022@gmail.com>
2025-04-10 08:58:15 +0900

    Implement Navbar

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import Navbar from "./components/navbar";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -44,6 +45,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
     return ( 
     <div>
+      <Navbar />
       <Outlet />
       </div>
     );
